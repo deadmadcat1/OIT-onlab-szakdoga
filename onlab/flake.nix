f95c42f2d25a777e48b8cb9c54a6e2e3495e2df8
deadmadcat1 <cuellarmagyaradam@gmail.com>
2026-02-28 13:06:26 +0100

    set up debugging in nvim

---
+++ onlab/flake.nix
@@ -0,0 +1,34 @@
+{
+	description = "Szakdoga környezet flake";
+	inputs = {
+		nixpkgs.url = "nixpkgs/nixos-25.11";
+	};
+	outputs = {nixpkgs, ...}:
+	let
+		system = "x86_64-linux";
+		pkgs = import nixpkgs { inherit system; };
+	in
+	{
+		devShells.${system}.default = pkgs.mkShell {
+			packages = with pkgs; [
+				gnumake
+				cmake
+				gcc
+
+				glfw
+				glew
+				glm
+
+				clang-tools
+				lldb
+				glsl_analyzer
+			];
+			env = {
+				GLFW_LIB = "${pkgs.glfw}";
+				GLEW_LIB = "${pkgs.glew.dev}";
+				GLEW_OUT = "${pkgs.glew.out}";
+				GLM_LIB = "${pkgs.glm}";
+			};
+		};
+	};
+}
